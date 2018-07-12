611a7b752aeed5ea3a8b14fdbab30c0416e93483
Alex MacNair <alex@alexmacnair.com>
2018-07-11 22:16:53 -0400

    Started about app

---
+++ about/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib import messages
+
+
+def index(request):
+    return render(request, 'about/index.html')
