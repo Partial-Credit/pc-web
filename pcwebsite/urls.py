373f56be968c9e6ac883c986e95824ea913e1cb1
Alex MacNair <alex@alexmacnair.com>
2018-07-13 17:19:14 -0400

    Changed url for admin page for improved security

--- pcwebsite/urls.py
+++ pcwebsite/urls.py
@@ -21,8 +21,7 @@ import django_cas_ng.views as cas_views
 urlpatterns = [
     path('', include('home.urls')),
     path('about/', include('about.urls')),
-    
-    path('admin/', admin.site.urls),
+    path('manage/', admin.site.urls),
     path('login/', cas_views.login, name='cas_ng_login'),
     path('logout/', cas_views.logout, name='cas_ng_logout'),
     path('accounts/callback', cas_views.callback, name='cas_ng_proxy_callback'),
