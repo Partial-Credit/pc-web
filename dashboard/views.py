dfbdd76ba6137a4f00bc659babfda47d88f5a659
Alex MacNair <alex@alexmacnair.com>
2018-08-22 15:16:40 -0400

    Configured app routing

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib import messages
 
-# Create your views here.
+def index(request):
+    return render(request, 'dashboard/index.html')
