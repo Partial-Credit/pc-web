3e9bb15595ff70b693e39bfef5884b79e9913f52
Alex MacNair <alex@alexmacnair.com>
2019-01-07 22:45:58 -0500

    Update CAS urls calls to use new version.

--- pcwebsite/settings/base.py
+++ pcwebsite/settings/base.py
@@ -59,6 +59,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django_cas_ng.middleware.CASMiddleware',
 
 )
 
@@ -95,7 +96,7 @@ CKEDITOR_CONFIGS = {
             ['RemoveFormat']
         ],
         'width' : '100%',
-        
+
     },
 }
 CKEDITOR_UPLOAD_PATH = "uploads/"
