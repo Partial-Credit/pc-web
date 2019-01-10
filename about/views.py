aa5de26f88134619dc5eacb5328e37dabfc8e0dc
Alex MacNair <alex@alexmacnair.com>
2019-01-09 22:50:03 -0500

    Update about page to filter out hidden and inactive members

--- about/views.py
+++ about/views.py
@@ -15,20 +15,21 @@ def index(request):
 	basses = list()
 	alumni = list()
 
-	members = Member.objects.all().order_by('last_name')
+	# Don't include member objects with the hidden flag set. (Used to hide admin panel user if present)
+	# Don't include member objects that have been "deleted" (had is_active set to False)
+	members = Member.objects.filter(hidden=False, is_active=True).order_by('last_name')
 	for member in members:
-		if not member.hidden: # Don't include member objects with the hidden flag set. (Used to hide admin panel user if present)
-			if(member.current_member):
-				if member.voice_part == 'S':
-					sopranos.append(member)
-				elif member.voice_part == 'A':
-					altos.append(member)
-				elif member.voice_part == 'T':
-					tenors.append(member)
-				elif member.voice_part == "B":
-					basses.append(member)
-			else:
-				alumni.append(member)
+		if(member.current_member):
+			if member.voice_part == 'S':
+				sopranos.append(member)
+			elif member.voice_part == 'A':
+				altos.append(member)
+			elif member.voice_part == 'T':
+				tenors.append(member)
+			elif member.voice_part == "B":
+				basses.append(member)
+		else:
+			alumni.append(member)
 
 	alumni.sort(key=lambda x: x.class_year, reverse=True)
 	members_list = [{"Sopranos":sopranos}, {"Altos":altos}, {"Tenors":tenors}, {"Basses":basses}]
