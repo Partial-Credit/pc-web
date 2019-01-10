6fdf08a024e2050b91f8de4f076e7bf97f570473
Alex MacNair <alex@alexmacnair.com>
2019-01-09 21:35:37 -0500

    Fix about page crash if a hidden user existed

--- about/views.py
+++ about/views.py
@@ -17,17 +17,19 @@ def index(request):
 
 	members = Member.objects.all().order_by('last_name')
 	for member in members:
-		if(member.current_member):
-			if member.voice_part == 'S':
-				sopranos.append(member)
-			elif member.voice_part == 'A':
-				altos.append(member)
-			elif member.voice_part == 'T':
-				tenors.append(member)
-			elif member.voice_part == "B":
-				basses.append(member)
-		else:
-			alumni.append(member)
+		if not member.hidden: # Don't include member objects with the hidden flag set. (Used to hide admin panel user if present)
+			if(member.current_member):
+				if member.voice_part == 'S':
+					sopranos.append(member)
+				elif member.voice_part == 'A':
+					altos.append(member)
+				elif member.voice_part == 'T':
+					tenors.append(member)
+				elif member.voice_part == "B":
+					basses.append(member)
+			else:
+				alumni.append(member)
+
 	alumni.sort(key=lambda x: x.class_year, reverse=True)
 	members_list = [{"Sopranos":sopranos}, {"Altos":altos}, {"Tenors":tenors}, {"Basses":basses}]
 	return render(request, 'about/index.html', {"members_list": members_list, "alumni_list": alumni})
